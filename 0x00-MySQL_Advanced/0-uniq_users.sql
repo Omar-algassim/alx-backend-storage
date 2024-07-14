6d31034dd34e740276add98f168e65ff8e8ad801
Omar-Algassim <omarggg2@gamil.com>
2024-07-15 01:59:37 +0300

    Comment fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---create table uniq_users
---create user table with id email and name
+-- create user table with id email and name.
+
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
