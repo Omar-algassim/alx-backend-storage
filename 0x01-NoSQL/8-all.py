69b745589e3cbc3c526a192e0598823e6d55f91e
Omar-Algassim <omarggg2@gamil.com>
2024-07-17 01:43:17 +0300

    list all documents

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""list all documents in collection"""
+
+
+def list_all(mongo_collection):
+    """list all documents in collection"""
+    return [doc for doc in mongo_collection.find()]
