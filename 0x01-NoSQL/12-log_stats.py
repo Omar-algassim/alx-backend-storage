970c3533273635dd9813fc600a205f50fa56c605
Omar-Algassim <omarggg2@gamil.com>
2024-07-17 03:12:05 +0300

    add if __name__ == "__main__":

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,16 +2,16 @@
 """log stats"""
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient(host="localhost", port=27017)
+    logs = client.logs.nginx
 
-client = MongoClient(host="localhost", port=27017)
-logs = client.logs.nginx
-
-print(logs.count_documents({}), "logs")
-print("Methods:")
-print("\tmethod GET:", logs.count_documents({"method": "GET"}))
-print("\tmethod POST:", logs.count_documents({"method": "POST"}))
-print("\tmethod PUT:", logs.count_documents({"method": "PUT"}))
-print("\tmethod PATCH:", logs.count_documents({"method": "PATCH"}))
-print("\tmethod DELETE:", logs.count_documents({"method": "DELETE"}))
-print(logs.count_documents(
-    {"method": "GET", "path": "/status"}), "status check")
+    print(logs.count_documents({}), "logs")
+    print("Methods:")
+    print("\tmethod GET:", logs.count_documents({"method": "GET"}))
+    print("\tmethod POST:", logs.count_documents({"method": "POST"}))
+    print("\tmethod PUT:", logs.count_documents({"method": "PUT"}))
+    print("\tmethod PATCH:", logs.count_documents({"method": "PATCH"}))
+    print("\tmethod DELETE:", logs.count_documents({"method": "DELETE"}))
+    print(logs.count_documents(
+        {"method": "GET", "path": "/status"}), "status check")
