cf355617105e4ca675c01b49d2a679e0dc6d9db8
Omar-Algassim <omarggg2@gamil.com>
2024-07-17 16:19:47 +0300

    implement cach class and store methode

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""redies exercise"""
+import redis
+import uuid
+from typing import Union
+
+class Cache():
+    """cache class"""
+    
+    def __init__(self):
+        """init"""
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+    
+    def store(self, data: Union[str, float, int, bytes]) -> str:
+        """store data with specific id"""
+        id = str(uuid.uuid4())
+        self._redis.set(id, data)
+        return id
